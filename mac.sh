6d816cd164192f31fde373d077cc3846faefa36b
Sang Goo Kang <sanggoo@doordash.com>
2017-09-22 08:43:59 -0700

    changed names, and started mac.sh

---
+++ mac.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install xcode command line tools
+xcode-select --install
+
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Update homebrew
+brew update && brew upgrade
+
+# Install vim
+brew install vim --with-python3 --with-override-system-vi
+
+# Install gnu utilities, git, and zsh
+brew install coreutils git zsh zsh-completions
+
+# Set zsh to be default shell
+sudo echo "$(which zsh)" >> /etc/shells
+chsh -s $(which zsh)
+
+#Install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+

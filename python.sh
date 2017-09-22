adf8b95eaed91afa35ea285248ff658f251b0f07
Sang Goo Kang <sanggoo@doordash.com>
2017-09-21 17:27:03 -0700

    minor changes

---
+++ python.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /tmp
+if [ "$(uname)" == "Darwin" ]; then
+  curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > Miniconda3.sh
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > Miniconda3.sh
+fi
+bash Miniconda3.sh
+
+

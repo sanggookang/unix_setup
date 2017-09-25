5f387f39c49ce77c4f46fc60458c688b779fcca4
Sang Goo Kang <sanggookang.sg@gmail.com>
2017-09-25 16:10:35 -0700

    minor changes to vim installation

--- vim/vim.sh
+++ vim/vim.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 OLDPATH=$PWD
-SCRIPTPATH="$BASH_SOURCE"
+SCRIPTPATH=`dirname $BASH_SOURCE`
 cd
 git clone https://github.com/vim/vim.git
+cd $OLDPATH
 ${SCRIPTPATH}/vim_update.sh
-cd OLDPATH
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 unset OLDPATH
-ln -s ${SCRIPTPATH}/.vimrc ~/.vimrc
+sudo cp ${SCRIPTPATH}/.vimrc ~/.vimrc
 vim +PluginInstall +qall
 unset SCRIPTPATH
